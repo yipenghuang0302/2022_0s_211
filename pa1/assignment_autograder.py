6b53a92d163e21d25f17cd0274fb15c44a1413e4
Yipeng Huang <yh804@ilab2.cs.rutgers.edu>
2022-01-24 17:12:02 -0500

    Grading infrastructure.

--- pa1/assignment_autograder.py
+++ pa1/assignment_autograder.py
@@ -1,9 +1,11 @@
 #!/usr/bin/python3
 
 from greedyScheduling import autograder as greedyScheduling_autograder
+from balanced import autograder as balanced_autograder
 
 total = 0
 
 total += greedyScheduling_autograder.grade_greedyScheduling ( path="greedyScheduling/", verbose=True )
+total += balanced_autograder.grade_balanced ( path="balanced/", verbose=True )
 
 print ("Score on assignment: {} out of 80.".format(total))

e57342d1e352a7ed7af6257d83588d01a7043d29
Yipeng Huang <yh804@ilab1.cs.rutgers.edu>
2022-04-07 13:01:05 -0400

    Adding cache simulation files for sumArray

--- pa5/sumArray/sumArray.c
+++ pa5/sumArray/sumArray.c
@@ -3,18 +3,28 @@
 #include <time.h>
 #include <assert.h>
 
-#define M 1024
+#define M 4
+
+#include <stddef.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <assert.h>
+
+/* Markers used to bound trace regions of interest */
+volatile char MARKER_START, MARKER_END;
 
 int sumArrayRows (
     int a[M][M]
 ) {
     int sum = 0;
 
+    MARKER_START = 211;
     for ( int i=0; i<M; i++ )
         for ( int j=0; j<M; j++ ) {
             // printf( "a[i][j]=%d\n", a[i][j] );
             sum += a[i][j];
         }
+    MARKER_END = 211;
 
     // printf( "sumArrayRows sum=%d\n", sum );
     return sum;
@@ -25,12 +35,69 @@ int sumArrayCols (
 ) {
     int sum = 0;
 
+    MARKER_START = 211;
     for ( int j=0; j<M; j++ )
         for ( int i=0; i<M; i++ )
             sum += a[i][j];
+    MARKER_END = 211;
 
     // printf( "sumArrayCols sum=%d\n", sum );
     return sum;
+}
+
+int main(int argc, char* argv[])
+{
+
+    // prepare random number generator
+    time_t t;
+    srand((unsigned) time(&t));
+
+    // prepare timing infrastructure
+    clock_t start, end;
+    double cpu_time_used;
+
+    /* Record marker addresses */
+    FILE* marker_fp = fopen(".marker","w");
+    assert(marker_fp);
+    fprintf(marker_fp, "%llx\n%llx",
+        (unsigned long long int) &MARKER_START,
+        (unsigned long long int) &MARKER_END );
+    fclose(marker_fp);
+
+    int a[M][M];
+    // INITIALIZE ARRAY WITH RANDOM NUMBERS
+    for ( int j=0; j<M; j++ )
+        for ( int i=0; i<M; i++ )
+            a[i][j] = rand() % 256;
+
+    // TIME sumArrayRows
+    start = clock();
+    int sumArrayRowsResult = sumArrayRows(a);
+    end = clock();
+    cpu_time_used = ((double) (end - start)) / CLOCKS_PER_SEC;
+    printf("sumArrayRows took %lf seconds.\n", cpu_time_used);
+
+    // TIME sumArrayCols
+    start = clock();
+    int sumArrayColsResult = sumArrayCols(a);
+    end = clock();
+    cpu_time_used = ((double) (end - start)) / CLOCKS_PER_SEC;
+    printf("sumArrayCols took %lf seconds.\n", cpu_time_used);
+
+    assert ( sumArrayRowsResult == sumArrayColsResult );
+
+    // TIME sumArrayRcrs
+    // start = clock();
+    // int sumArrayRcrsResult = sumArrayRcrs( M, 0, 0, a );
+    // end = clock();
+    // cpu_time_used = ((double) (end - start)) / CLOCKS_PER_SEC;
+    // printf("sumArrayRcrs took %lf seconds.\n", cpu_time_used);
+
+    // assert ( sumArrayRcrsResult == sumArrayRowsResult );
+    // assert ( sumArrayRcrsResult == sumArrayColsResult );
+
+    exit(EXIT_SUCCESS);
+
 }
 
 // int sumArrayRcrs (
@@ -57,53 +124,3 @@ int sumArrayCols (
 //         return sum;
 //     }
 // }
-
-int main() {
-
-    time_t t;
-    srand((unsigned) time(&t));
-
-    int a[M][M];
-
-    // INITIALIZE ARRAY WITH RANDOM NUMBERS
-    for ( int j=0; j<M; j++ )
-        for ( int i=0; i<M; i++ )
-            a[i][j] = rand() % 256;
-
-    clock_t start, end;
-    double cpu_time_used;
-
-    // TIME sumArrayRcrs
-    // start = clock();
-    // int sumArrayRcrsResult = sumArrayRcrs( M, 0, 0, a );
-    // end = clock();
-    // cpu_time_used = ((double) (end - start)) / CLOCKS_PER_SEC;
-    // printf("sumArrayRcrs took %lf seconds.\n", cpu_time_used);
-
-
-    // assert ( sumArrayRcrsResult == sumArrayRowsResult );
-
-    // TIME sumArrayRows
-    start = clock();
-    int sumArrayRowsResult = sumArrayRows(a);
-    end = clock();
-    cpu_time_used = ((double) (end - start)) / CLOCKS_PER_SEC;
-    printf("sumArrayRows took %lf seconds.\n", cpu_time_used);
-
-
-    // TIME sumArrayCols
-    start = clock();
-    int sumArrayColsResult = sumArrayCols(a);
-    end = clock();
-    cpu_time_used = ((double) (end - start)) / CLOCKS_PER_SEC;
-    printf("sumArrayCols took %lf seconds.\n", cpu_time_used);
-
-
-
-
-
-    // assert ( sumArrayRcrsResult == sumArrayColsResult );
-    assert ( sumArrayRowsResult == sumArrayColsResult );
-
-    return EXIT_SUCCESS;
-}

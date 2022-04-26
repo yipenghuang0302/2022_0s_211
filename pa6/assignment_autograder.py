15e592762933985ad51a3624ba211e346efafd68
Yipeng Huang <yh804@ilab3.cs.rutgers.edu>
2022-04-26 10:00:17 -0400

    Adding option for extra credit.

--- pa6/assignment_autograder.py
+++ pa6/assignment_autograder.py
@@ -3,6 +3,7 @@
 from comparator import autograder as comparator_autograder
 from sevenSegmentDisplayE import autograder as sevenSegmentDisplayE_autograder
 from sevenSegmentDisplayC import autograder as sevenSegmentDisplayC_autograder
+from sevenSegmentDisplay import autograder as sevenSegmentDisplay_autograder
 from fullyAssociative import autograder as fullyAssociative_autograder
 from setAssociative import autograder as setAssociative_autograder
 from binSub import autograder as binSub_autograder
@@ -12,6 +13,7 @@ total = 0
 total += comparator_autograder.grade_comparator ( path="comparator/", verbose=False )
 total += sevenSegmentDisplayE_autograder.grade_sevenSegmentDisplayE ( path="sevenSegmentDisplayE/", verbose=False )
 total += sevenSegmentDisplayC_autograder.grade_sevenSegmentDisplayC ( path="sevenSegmentDisplayC/", verbose=False )
+total += sevenSegmentDisplay_autograder.grade_sevenSegmentDisplay ( path="sevenSegmentDisplay/", verbose=False )
 total += fullyAssociative_autograder.grade_fullyAssociative ( path="fullyAssociative/", verbose=False )
 total += setAssociative_autograder.grade_setAssociative ( path="setAssociative/", verbose=False )
 total += binSub_autograder.grade_binSub ( path="binSub/", verbose=False )
